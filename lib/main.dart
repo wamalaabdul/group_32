923775de070318f5868f9159c8003668df2624a3
WAMALA.ABDUL.JUNIOR <oscartyson132@gmail.com>
2024-07-04 10:57:04 +0000

    setting up the app structure

--- lib/main.dart
+++ lib/main.dart
@@ -3,40 +3,27 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'Agrochemical App',
       theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('Agrochemical App'),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+        child: Text('Welcome to Agrochemical App!'),
       ),
     );
   }
